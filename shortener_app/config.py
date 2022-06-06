f53ecd7d3d256843a45e06cbdfc2369800aeb031
Aditya Yadav <yaditya20@gmail.com>
2022-06-07 00:21:39 +0530

    Project Setup and Configuration
    1. Created the Project's Folder Structure
    2. Added Project Dependencies
    3. Defined Environment Variables

---
+++ shortener_app/config.py
@@ -0,0 +1,15 @@
+# shortener_app/config.py
+
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    env_name: str = "Local"
+    base_url: str = "http://localhost:8000"
+    db_url: str = "sqlite:///./shortener.db"
+
+
+def get_settings() -> Settings:
+    settings = Settings()
+    print(f"Loading settings for: {settings.env_name}")
+    return settings
